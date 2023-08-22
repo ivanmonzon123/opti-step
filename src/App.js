89a09f9224f4d9cc8b60767dc2b69058028ad060
Ivan Monzon <monzonyujraivanfranklin@gmail.com>
2023-08-22 09:53:11 -0400

    Added react-router-dom, NavBarComp and HomePage

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
-import React, { useState } from 'react';
-
-/* global solver */
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBarComp from "./components/NavBarComp";
+import HomePage from "./pages/HomePage"
+import './App.css';
 function App() {
-  const [result, setResult] = useState(null);
-
-  const solveProblem = () => {
-    let model = {
-      optimize: "precio",
-      opType: "max",
-      constraints: {
-        "acabado": {"min": 100},
-        "carpinteria": {"max": 80},
-        "demanda": {"max": 40}
-      },
-      variables: {
-        bus: {
-          "precio": 3,
-          "acabado": 2,
-          "carpinteria": 1,
-          "demanda": 1
-        },
-        tren: {
-          "precio": 2,
-          "acabado": 1,
-          "carpinteria": 1,
-          "demanda": 0
-        }
-      }
-    }
-
-    // Use 'Solver' from the global scope (CDN)
-    const solution = solver.Solve(model);
-    setResult(solution);
-    console.log(solution)
-  };
-
   return (
-      <div>
-        <h1>Linear Programming Solver</h1>
-        <button onClick={solveProblem}>Solve Problem</button>
-        {result && (
-            <div>
-              <h2>Results:</h2>
-              <p>Maximized Value: {result.result}</p>
-              <p>x: {result.bus}</p>
-              <p>y: {result.tren}</p>
-            </div>
-        )}
-      </div>
+      <BrowserRouter>
+        <article className="app-head-container">
+          <NavBarComp/>
+        </article>
+
+        <article className="app-body-container">
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          </Routes>
+        </article>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBarComp.js
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import OptiStepLogo from '../images/os-logo.png';
+import "../styles/components/NavBarComp.css"
+
+export default function NavBarComp() {
+  return (
+      <Navbar className="nav-bar-container" expand="lg" bg="dark" variant="dark">
+        <Container className="nav-bar-content">
+          <Navbar.Brand href="/">
+            <img className="nav-bar-logo" src={OptiStepLogo} alt="OptiStep-Logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Acerca de</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  );
+}

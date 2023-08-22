7ccb344906d05cc259ac8a973524d04cebfa8827
Ivan Monzon <monzonyujraivanfranklin@gmail.com>
2023-08-21 21:09:19 -0400

    added javascript-lp-solver library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+/* global solver */
 function App() {
+  const [result, setResult] = useState(null);
+
+  const solveProblem = () => {
+    let model = {
+      optimize: "precio",
+      opType: "max",
+      constraints: {
+        "acabado": {"min": 100},
+        "carpinteria": {"max": 80},
+        "demanda": {"max": 40}
+      },
+      variables: {
+        bus: {
+          "precio": 3,
+          "acabado": 2,
+          "carpinteria": 1,
+          "demanda": 1
+        },
+        tren: {
+          "precio": 2,
+          "acabado": 1,
+          "carpinteria": 1,
+          "demanda": 0
+        }
+      }
+    }
+
+    // Use 'Solver' from the global scope (CDN)
+    const solution = solver.Solve(model);
+    setResult(solution);
+    console.log(solution)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Linear Programming Solver</h1>
+        <button onClick={solveProblem}>Solve Problem</button>
+        {result && (
+            <div>
+              <h2>Results:</h2>
+              <p>Maximized Value: {result.result}</p>
+              <p>x: {result.bus}</p>
+              <p>y: {result.tren}</p>
+            </div>
+        )}
+      </div>
   );
 }
 

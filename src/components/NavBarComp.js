e596d9ef348c2a3146932104986105b03bcdf4f1
Ivan Monzon <monzonyujraivanfranklin@gmail.com>
2023-08-22 22:08:06 -0400

    Added type and sizes to txt

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -1,36 +1,27 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import OptiStepLogo from '../images/os-logo.png';
 import "../styles/components/NavBarComp.css"
+import {Link} from "react-router-dom";
 
 export default function NavBarComp() {
   return (
-      <Navbar className="nav-bar-container" expand="lg" bg="dark" variant="dark">
-        <Container className="nav-bar-content">
-          <Navbar.Brand href="/">
-            <img className="nav-bar-logo" src={OptiStepLogo} alt="OptiStep-Logo"/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#link">Acerca de</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <article className="nav-bar-container">
+        <Navbar expand="lg" bg="dark" variant="dark">
+            <Link to="/">
+              <img className="nav-bar-logo" src={OptiStepLogo} alt="OptiStep-Logo"/>
+            </Link>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="nav-bar-options">
+                <Link to="/">Inicio</Link>
+
+                <Link to="/about">Acerca de</Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      </article>
   );
 }

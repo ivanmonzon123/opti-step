2f9b92e18d293965e4a7f9f4fdb5ef2a8f5ff6b8
Ivan Monzon <monzonyujraivanfranklin@gmail.com>
2023-08-23 09:37:23 -0400

    Added base to home-page, default theme, and shadows

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -6,19 +6,19 @@ import {Link} from "react-router-dom";
 
 export default function NavBarComp() {
   return (
-      <article className="nav-bar-container">
-        <Navbar expand="lg" bg="dark" variant="dark">
+      <article className="navbar-container">
+        <Navbar expand="lg" variant="dark">
             <Link to="/">
-              <img className="nav-bar-logo" src={OptiStepLogo} alt="OptiStep-Logo"/>
+              <img className="navbar-logo" src={OptiStepLogo} alt="OptiStep-Logo"/>
             </Link>
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="nav-bar-options">
-                <Link to="/">Inicio</Link>
+              <Nav className="navbar-options">
+                <Link to="/" className="os-txt os-txt-md">Inicio</Link>
 
-                <Link to="/about">Acerca de</Link>
+                <Link to="/about" className="os-txt os-txt-md">Acerca de</Link>
               </Nav>
             </Navbar.Collapse>
         </Navbar>
